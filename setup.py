f9a9bce5d9f33535b0eddabc5422ac5f89241a9b
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2023-02-23 15:24:18 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
+
 setup(
-  name = 'artificial-neural-network',
-  packages = ['artificial-neural-network'],
-  version = '0.1',
-  license='gpl-3.0',
-  description = 'Easy to use artificial neural network library in Python based from Keras library.',
-  author = 'Ivan Dustin Bilon',
-  author_email = 'ivan22.dust@gmail.com',
-  url = 'https://github.com/ivandustin/neural',
-  keywords = ['easy to use', 'artificial neural network', 'beginners'],
-  install_requires=[
-          'keras',
-          'tensorflow',
-      ]
+    name = 'artificial-neural-network',
+    packages = ['neural'],
+    version = '0.2',
+    license ='gpl-2.0',
+    description = 'Easy to use artificial neural network library in Python based from Keras library',
+    author = 'Ivan Dustin B. Bilon',
+    author_email = 'ivan22.dust@gmail.com',
+    url = 'https://github.com/ivandustin/neural',
+    keywords = ['easy to use', 'artificial neural network', 'beginners'],
+    install_requires=[
+        'keras',
+        'tensorflow',
+    ]
 )
